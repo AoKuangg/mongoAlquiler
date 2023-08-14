89c215bcfbae4dafaa35a4790911bb01574add4e
JuanCamiloPaezArizaCAMPUS <camipa2305@gmail.com>
2023-08-14 15:21:15 -0500

    feat: :sparkles: Se agregan los endpoints de cliente y automovil
    Se realiza tan rapido ya que tenia las querys de antes

---
+++ routers/automovil.js
@@ -0,0 +1,72 @@
+import { Router } from "express";
+import { connectDB } from "../db/conexion.js";
+import { limitRequests } from "../helpers/limit.js";
+
+const Automovil = Router();
+let db = await connectDB();
+
+Automovil.use(limitRequests);
+
+Automovil.get("/", async (req, res) => {
+  try {
+    const collection = db.collection("automovil");
+    const data = await collection
+      .aggregate([
+        {
+          $match: { estado: "DISPONIBLE" },
+        },
+        {
+          $lookup: {
+            from: "automovil",
+            localField: "ID_automovil",
+            foreignField: "_id",
+            as: "Automoviles disponibles",
+          },
+        },
+        {
+          $project: {
+            ID_automovil: 0,
+          },
+        },
+      ])
+      .toArray();
+    res.send(data);
+  } catch (error) {
+    es.status(500).json({
+      message: "Error al listar los automoviles",
+      error: error.message,
+    });
+  }
+});
+
+Automovil.get("/capacidad", async (req, res) => {
+  try {
+    const collection = db.collection("automovil");
+    const data = await collection.find({ capacidad: { $gte: 5 } });
+    res.send(data);
+  } catch (error) {
+    es.status(500).json({
+      message: "Error al listar los automoviles",
+      error: error.message,
+    });
+  }
+});
+
+
+Automovil.get("/sort", async (req, res) => {
+  try {
+    const collection = db.collection("automovil");
+    const data = await collection.find().sort({
+      marca: 1,
+      modelo: -1,
+    });
+    res.send(data);
+  } catch (error) {
+    es.status(500).json({
+      message: "Error al listar los automoviles",
+      error: error.message,
+    });
+  }
+});
+
+export default Automovil;
